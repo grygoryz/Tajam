56ee09299473a9521f431385f5d1744c7350aa73
grygoryz <grigoryts@yahoo.com>
2020-05-25 19:49:08 +0300

    update webpack bundle

---
+++ src/index.js
@@ -0,0 +1,5 @@
+import "./scripts/init"
+import "./styles/styles.scss"
+
+
+
--- webpack.config.js
+++ webpack.config.js
@@ -1,75 +1,156 @@
-const path = require('path');
-const MiniCssExtractPlugin = require("mini-css-extract-plugin");
-const LiveReloadPlugin = require('webpack-livereload-plugin');
+const path = require("path");
+const HTMLWebpackPlugin = require("html-webpack-plugin");
+const {CleanWebpackPlugin} = require("clean-webpack-plugin");
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const OptimizeCssAssetsWebpackPlugin = require("optimize-css-assets-webpack-plugin");
+const TerserWebpackPlugin = require("terser-webpack-plugin");
+
+const isDev = process.env.NODE_ENV === "development";
+const isProd = !isDev;
+
+const optimization = () => {
+    const config = {
+        splitChunks: {
+            chunks: "all"
+        }
+    };
+    if (isProd){
+        config.minimizer = [
+            new OptimizeCssAssetsWebpackPlugin(),
+            new TerserWebpackPlugin()
+        ]
+    }
+    return config;
+};
+
+const filename = ext => isDev ? `[name].${ext}` : `[name].[hash].${ext}`;
+
+const cssLoaders = extra => {
+    const loaders = [{
+        loader: MiniCssExtractPlugin.loader,
+        options: {
+            hmr: isDev,
+            reloadAll: true
+        }
+    } ,"css-loader"];
+
+    if (extra){
+        loaders.push(extra);
+    }
+
+    return loaders;
+};
+
+const babelOptions = (preset) => {
+  const opts =  {
+      presets: [
+          "@babel/preset-env",
+      ],
+      plugins: [
+          "@babel/plugin-proposal-class-properties"
+      ]
+  };
+
+  if (preset){
+      opts.presets.push(preset);
+  }
+
+   return opts;
+};
+
+const jsLoaders = () => {
+    const loaders = [{
+            loader: "babel-loader",
+            options: babelOptions()
+        }];
+
+    if (isDev){
+        loaders.push("eslint-loader");
+    }
+
+  return loaders;
+};
+
 module.exports = {
-    entry: './src/javascript/index.js',
-    output: {
-        path: path.resolve(__dirname, 'dist'),
-        filename: 'bundle.js'
+    context: path.resolve(__dirname, "src"),
+    mode: "development",
+    entry: {
+        main: ['@babel/polyfill','./index.js']
     },
-    mode: 'development',
-    
+    output: {
+        filename: filename("js"),
+        path: path.resolve(__dirname, 'dist')
+    },
+    optimization: optimization(),
+    devtool: isDev ? "source-map" : "",
+    devServer: {
+        port: 4200,
+        hot: isDev
+    },
+    plugins: [
+        new HTMLWebpackPlugin({
+            template: "./index.html",
+            minify: {
+                collapseWhitespace: isProd
+            }
+        }),
+        new CleanWebpackPlugin(),
+        new CopyWebpackPlugin({
+            patterns: [
+                {
+                    from: path.resolve(__dirname, "src/images/"),
+                    to: path.resolve(__dirname, "dist/images")
+                }
+            ]
+        }),
+        new MiniCssExtractPlugin({
+            filename: filename("css")
+        })
+    ],
     module: {
         rules: [
             {
+                test: /\.css$/,
+                use: cssLoaders()
+            },
+            {
+                test: /\.(png|jpe?g|svg|gif)$/,
+                use: ["file-loader"]
+            },
+            {
+                test: /\.(ttf|woff|woff2|eot)$/,
+                use: ["file-loader"]
+            },
+            {
+                test: /\.xml$/,
+                use: ["xml-loader"]
+            },
+            {
+                test: /\.s[ac]ss$/,
+                use: cssLoaders("sass-loader")
+            },
+            {
                 test: /\.js$/,
-                exclude: /(node_modules)/,
-                use: {
-                    loader: 'babel-loader',
-                    options: {
-                        presets: ['@babel/preset-env']
-                    }
+                exclude: /node_modules/,
+                use: jsLoaders()
+            },
+            {
+                test: /\.ts$/,
+                exclude: /node_modules/,
+                loader: {
+                    loader: "babel-loader",
+                    options: babelOptions("@babel/preset-typescript")
                 }
             },
             {
-                test: /\.(sa|sc|c)ss$/,
-                use: [
-                    {
-                        loader: MiniCssExtractPlugin.loader
-                    },
-                    {
-                        loader: "css-loader",
-                    },
-                    {
-                        loader: "postcss-loader"
-                    },
-                    {
-                        loader: "sass-loader",
-                        options: {
-                            implementation: require("sass")
-                        }
-                    }
-                ]
-            },
-            {
-                test: /\.(png|jpe?g|gif|svg)$/,
-                use: [
-                    {
-                        loader: "file-loader",
-                        options: {
-                            outputPath: 'images'
-                        }
-                    }
-                ]
-            },
-            {
-                test: /\.(woff|woff2|ttf|otf|eot)$/,
-                use: [
-                    {
-                        loader: "file-loader",
-                        options: {
-                            outputPath: 'fonts'
-                        }
-                    }
-                ]
+                test: /\.jsx$/,
+                exclude: /node_modules/,
+                loader: {
+                    loader: "babel-loader",
+                    options: babelOptions("@babel/preset-react")
+                }
             }
         ]
-    },
-    
-    plugins: [
-        new MiniCssExtractPlugin({
-            filename: "bundle.css"
-        }),
-        new LiveReloadPlugin()
-    ]
+    }
 };
